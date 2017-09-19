51fb38fff7ddc7bb5a38b2cec4f1aa9f06e97207
ermankuruoglu <ermankuruoglu@hotmail.com>
2017-09-19 15:51:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
-import express from 'express';
-import morgan from 'morgan';
-import mongoose from 'mongoose';
-import router from './router';
+// import express from 'express';
+// import morgan from 'morgan';
+// import mongoose from 'mongoose';
+// import router from './router';
+//
+// // Connect to MongoDB
+// mongoose.connect('mongodb://localhost/events');
+//
+// // Initialize http server
+// const app = express();
+//
+// // Logger that outputs all requests into the console
+// app.use(morgan('combined'));
+// // Use v1 as prefix for all API endpoints
+// app.use('/v1', router);
+//
+// // Launch the server on port 3000
+// const server = app.listen(3000, () => {
+//   const { address, port } = server.address();
+//   console.log(`Listening at http://${address}:${port}`);
+// });
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost/events');
+import express from 'express';
 
 // Initialize http server
 const app = express();
 
-// Logger that outputs all requests into the console
-app.use(morgan('combined'));
-// Use v1 as prefix for all API endpoints
-app.use('/v1', router);
+// Handle / route
+app.get('/', (req, res) =>
+  res.send('Hello World!')
+)
 
 // Launch the server on port 3000
 const server = app.listen(3000, () => {
